70c09f579aa9ba97c738d8ccd9c0cdb0133b1124
Oli Ollerenshaw <oli@mroshaw.co.uk>
2025-05-08 14:59:19 +0100

    Refactored 'anchor' to 'pivot' for consistency

--- Runtime/Scripts/Editor/EditorTools/InitialiseBuildingEditorTool.cs
+++ Runtime/Scripts/Editor/EditorTools/InitialiseBuildingEditorTool.cs
@@ -12,12 +12,13 @@ using DaftAppleGames.Attributes;
 namespace DaftAppleGames.BuildingTools.Editor
 {
     /// <summary>
-    /// This tool sets up the building, adding the Building component and re-jigging the anchor so that the building can be placed easier
+    /// This tool sets up the building, adding the Building component and re-jigging the pivot so that the building can be placed easier
     /// </summary>
     [CreateAssetMenu(fileName = "InitialiseBuildingEditorTool", menuName = "Daft Apple Games/Building Tools/Initialise Building Tool")]
     internal class InitialiseBuildingEditorTool : BuildingEditorTool
     {
-        [SerializeField] [BoxGroup("Settings")] internal float adjustAnchorHeight;
+        [SerializeField] [BoxGroup("Settings")]
+        [Tooltip("Drops the pivot by this amount, effectively raising the building. This makes it easier to place on uneven terrain.")] private float adjustPivotHeight;
 
         protected override string GetToolName()
         {
@@ -56,7 +57,7 @@ namespace DaftAppleGames.BuildingTools.Editor
             log.AddToLog(LogLevel.Info, "Adding Building component (if not already there). DONE!");
 
             log.AddToLog(LogLevel.Info, "Setting Building anchor...");
-            SetBuildingAnchor();
+            SetBuildingPivot();
             log.AddToLog(LogLevel.Info, "Setting Building anchor... DONE!");
         }
 
@@ -75,12 +76,12 @@ namespace DaftAppleGames.BuildingTools.Editor
         /// Adjusts the building anchor, by parenting the building, so that it is raised up over the anchor point
         /// This makes it easier to place on a non-flat surface or terrain
         /// </summary>
-        private void SetBuildingAnchor()
+        private void SetBuildingPivot()
         {
             // Move each child game object vertically by the settings amount
             foreach (Transform child in SelectedGameObject.transform)
             {
-                Vector3 newPosition = new(0, adjustAnchorHeight, 0);
+                Vector3 newPosition = new(child.localPosition.x, adjustPivotHeight, child.localPosition.z);
                 // child.position += Vector3.up * buildingEditorSettings.adjustAnchorHeight;
                 child.localPosition = newPosition;
                 log.AddToLog(LogLevel.Debug, $"Moved child {child.name} to position {newPosition}.");
